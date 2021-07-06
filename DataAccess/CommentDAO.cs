682b58ca383c51698ae6b1f245b0062ff2cc85a9
chiuycuong <77531153+chiuycuong@users.noreply.github.com>
2021-07-06 15:16:07 +0700

    Revert "CommentRepo, BookmarkRepo"
    This reverts commit 0e2125d1060ec754da202604a49eadff22ae6993.

--- DataAccess/CommentDAO.cs
+++ DataAccess/CommentDAO.cs
@@ -43,7 +43,7 @@ namespace DataAccess
             return _comment;
         }
 
-        public IEnumerable<Comment> GetCommentsByPosts(int postId)
+        public IEnumerable<Comment> GetCommentsByPosts(Post post)
         {
             List<Comment> lst = new List<Comment>();
             try
