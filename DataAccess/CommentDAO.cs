8256b11e5c1520dc66308249cfcfdf091ffec524
chiuycuong <77531153+chiuycuong@users.noreply.github.com>
2021-07-06 15:16:07 +0700

    Revert "CommentRepo, BookmarkRepo"
    This reverts commit 0e2125d1060ec754da202604a49eadff22ae6993.

--- DataAccess/CommentDAO.cs
+++ DataAccess/CommentDAO.cs
@@ -43,6 +43,76 @@ namespace DataAccess
             return _comment;
         }
 
+        public IEnumerable<Comment> GetCommentsByPosts(Post post)
+        {
+            try
+            {
+                using var context = new PRN211_OnlyFunds_CopyContext();
+                return context.Comments.Where(cmt => cmt.PostId == post.PostId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void AddComment(Comment comment)
+        {
+            try
+            {
+                using var context = new PRN211_OnlyFunds_CopyContext();
+                context.Comments.Add(comment);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void DeleteComment(Comment comment)
+        {
+            try
+            {
+                using var context = new PRN211_OnlyFunds_CopyContext();
+                context.Comments.Remove(comment);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void EditComment(Comment updatedComment)
+        {
+            try
+            {
+                using var context = new PRN211_OnlyFunds_CopyContext();
+                context.Entry(updatedComment).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Comment GetCommentByID(int commentID)
+        {
+            Comment _comment = null;
+            try
+            {
+                using var context = new PRN211_OnlyFunds_CopyContext();
+                _comment = context.Comments.Where(cmt => cmt.CommentId == commentID).Single();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return _comment;
+        }
+
         public IEnumerable<Comment> GetCommentsByPosts(int postId)
         {
             List<Comment> lst = new List<Comment>();
