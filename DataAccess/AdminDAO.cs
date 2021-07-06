e6574e444937c2268f9a14af2bd12627655b9731
dangtrannam <dangtrannam2001@gmail.com>
2021-07-06 22:52:37 +0700

    Added adminDAO

---
+++ DataAccess/AdminDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+
+namespace DataAccess
+{
+    public class AdminDAO
+    {
+        public Admin CheckLogin(string username, string password)
+        {
+            Admin user = null;
+            try
+            {
+                using var context = new PRN211_OnlyFunds_CopyContext();
+                user = (Admin)context.Admins.Where(u => u.Username.Equals(username) && u.Password.Equals(password)).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return user;
+        }
+    }
+}
