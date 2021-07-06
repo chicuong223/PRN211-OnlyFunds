9f3466ec8574a513a1a449ca7ae6c8ab5570582a
chiuycuong <77531153+chiuycuong@users.noreply.github.com>
2021-07-06 15:16:07 +0700

    Revert "CommentRepo, BookmarkRepo"
    This reverts commit 0e2125d1060ec754da202604a49eadff22ae6993.

--- DataAccess/IRepository/IUserCategoryMapRepository.cs
+++ DataAccess/IRepository/IUserCategoryMapRepository.cs
@@ -1,13 +1,13 @@
-﻿using BusinessObjects;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObjects;
 
 namespace DataAccess.IRepository
 {
-    public interface IUserCategoryMapRepository
+    interface IUserCategoryMapRepository
     {
         void AddUserCategoryMap(UserCategoryMap map);
         UserCategoryMap GetUserCategoryMap(string username, int categoryId);
