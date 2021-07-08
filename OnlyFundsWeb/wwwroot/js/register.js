226bced6304b56377aed899d6b19b5925c0b8e53
Wolf1910 <66056386+Wolf1910@users.noreply.github.com>
2021-07-08 20:15:25 +0700

    Javascript cho register

--- OnlyFundsWeb/wwwroot/js/register.js
+++ OnlyFundsWeb/wwwroot/js/register.js
@@ -1,111 +1,130 @@
-
-
-const firstname = document.getElementById('firstname');
-const lastname = document.getElementById("lastname");
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const confirmPassword = document.getElementById("confPass");
-const email = document.getElementById("email");
+let fname_error = document.getElementById('fnameError');
+let lname_error = document.getElementById("lnameError");
+let username_error = document.getElementById("userenameError");
+let email_error = document.getElementById("emailError");
+let pass_error = document.getElementById("passwordError");
+let confPass_error = document.getElementById("confPassError");
 const usernameRegex = /^[a-z0-9A-Z]{8,32}$/;
-//const REGISTER_FORM = document.getElementById("register-btn");
+const nameRegex = /^[a-zA-Z]{1,}$/;
+const passRegex = /(?=.{6,})/;
+const emailRegex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-//REGISTER_FORM.addEventListener(onsubmit, validateForm());
-//
-//function validateForm() {
-//    let fname = document.getElementById("firstname");
-//    let lname = document.getElementById("lastname");
-//    let username = document.getElementById("username");
-//    let password = document.getElementById("password");
-//    let email = document.getElementById("email");
-//    
-//    
 
-function validateName() {
-    console.log(this.value);
-    this.style.background = "yellowGreen";
-    const NAME_REGEX = /^(^[a-zA-Z]+)$/; // does not allow names to contain numbers
-
-    if (this.value.match(/^ *$/) !== null) {
-        this.style.background = "pink";
-        console.log("emp");
-    }
-    if (!NAME_REGEX.test(String(name.value))) {
-        this.style.background = "pink";
-        console.log("num");
-    }
-}
-
-function validateInput(input, regex) {
-    let valid = regex.test(input);
-    return valid;
-}
-if (firstname) {
-    firstname.addEventListener('blur', validateName);
-}
-//
-if (firstname) {
-    firstname.addEventListener('focus', function () {
-        this.style.background = "white";
-    });
-}
-//
-if (lastname) {
-    lastname.addEventListener('blur', validateName);
-}
-//
-if (lastname) {
-    lastname.addEventListener('focus', function () {
-        this.style.background = "white";
-    });
-}
-
-document.getElementById('register-form').addEventListener('submit', event => {
-    const usernameRegex = /^([a-z0-9A-Z]+){8,32}$/;
-    const nameRegex = /[a-zA-Z]/;
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (!validateInput(username.value, usernameRegex)
-            || !validateInput(firstname.value, nameRegex)
-            || !validateInput(lastname.value, nameRegex)
-            || !validateInput(email.value, emailRegex)
-            || password.value.length < 6
-            || confirmPassword.value !== password.value) {
-    }
-    if (!validateInput(username.value, usernameRegex)) {
-        event.preventDefault();
-        console.log("username error");
-        document.getElementById('usernameError').textContent = "Invalid username";
+function blockSpaceKey() {
+    if (event.keyCode == 32) {
         return false;
-    } else {
-        document.getElementById('usernameError').textContent = "";
     }
-    if (!validateInput(email.value, emailRegex)) {
-        event.preventDefault();
-        document.getElementById('emailError').textContent = "Invalid email";
-        console.log("email error");
+}
+
+function validate(parameter) {
+    if (parameter === 'username') {
+        const uname = document.getElementById('username').value;
+        if (usernameRegex.test(uname) == false) {
+            username_error.innerHTML =
+                '<i class="fas fa-times"></i> Username is not valid, try again';
+            username_error.style.display = "block";
+            username_error.className = "error";
+            username_error.style.color = "red";
+        } else {
+            username_error.innerHTML =
+                '<i class="fas fa-check"></i> Username is valid';
+            username_error.style.display = "block";
+            username_error.className = "success";
+            username_error.style.color = "rgb(97 218 16)";
+        }
+    } else if (parameter === 'fname') {
+        const fname = document.getElementById('firstname').value;
+        if (nameRegex.test(fname) == false) {
+            fname_error.innerHTML =
+                '<i class="fas fa-times"></i> First name is not valid, try again'
+            fname_error.style.display = "block";
+            fname_error.className = "error";
+            fname_error.style.color = "red";
+        } else {
+            fname_error.innerHTML =
+                '<i class="fas fa-check"></i> First name is valid'
+            fname_error.style.display = "block";
+            fname_error.className = "success";
+            fname_error.style.color = "rgb(97 218 16)";
+        }
+    } else if (parameter === 'lname') {
+        const lname = document.getElementById('lastname').value;
+        if (nameRegex.test(lname) == false) {
+            lname_error.innerHTML =
+                '<i class="fas fa-times"></i> Last name is not valid, try again'
+            lname_error.style.display = "block";
+            lname_error.className = "error";
+            lname_error.style.color = "red";
+        } else {
+            lname_error.innerHTML =
+                '<i class="fas fa-check"></i> Last name is valid'
+            lname_error.style.display = "block";
+            lname_error.className = "success";
+            lname_error.style.color = "rgb(97 218 16)";
+        }
+    } else if (parameter === 'email') {
+        const email = document.getElementById('email').value;
+        if (emailRegex.test(email) == false) {
+            email_error.innerHTML =
+                '<i class="fas fa-times"></i> Email is not valid, try again'
+            email_error.style.display = "block";
+            email_error.className = "error";
+            email_error.style.color = "red";
+        } else {
+            email_error.innerHTML =
+                '<i class="fas fa-check"></i> Email is valid'
+            email_error.style.display = "block";
+            email_error.className = "success";
+            email_error.style.color = "rgb(97 218 16)";
+        }
+    } else if (parameter === 'password') {
+        const password = document.getElementById('password').value;
+        if (passRegex.test(password) == false) {
+            pass_error.innerHTML =
+                '<i class="fas fa-times"></i> Password is not valid, try again'
+            pass_error.style.display = "block";
+            pass_error.className = "error";
+            pass_error.style.color = "red";
+        } else {
+            pass_error.innerHTML =
+                '<i class="fas fa-check"></i> Password is valid'
+            pass_error.style.display = "block";
+            pass_error.className = "success";
+            pass_error.style.color = "rgb(97 218 16)";
+        }
+    } else if (parameter === 'confPass') {
+        const pass = document.getElementById('password').value;
+        const confPass = document.getElementById('confPass').value;
+        if (confPass !== pass) {
+            confPass_error.innerHTML =
+                '<i class="fas fa-times"></i> Password does not match'
+            confPass_error.style.display = "block";
+            confPass_error.className = "error";
+            confPass_error.style.color = "red";
+        } else {
+            confPass_error.innerHTML =
+                '<i class="fas fa-check"></i> Password matched'
+            confPass_error.style.display = "block";
+            confPass_error.className = "success";
+            confPass_error.style.color = "rgb(97 218 16)";
+        }
+    }
+}
+
+document.getElementById('reg-form').addEventListener('submit', e => {
+    const isError = document.querySelectorAll("span.error");
+    console.log(isError);
+    if (isError.length == 0) {
+        alert("Succesfully registerd");
+    }
+    else {
+        alert('There is(are) invalid input(s), try again!');
+        e.preventDefault();
         return false;
-    } else {
-        document.getElementById('emailError').textContent = "";
-    }
-    if (password.value.length < 6) {
-        console.log("password error");
-        event.preventDefault();
-        document.getElementById('passwordError').textContent = "Password length must be longer than 6 characters";
-        return false;
-    } else {
-        document.getElementById('passwordError').textContent = "";
-    }
-    if (confirmPassword.value !== password.value) {
-        console.log("confpass error");
-        event.preventDefault();
-        document.getElementById('confPassError').textContent = "Confirm password does not match";
-    } else {
-        document.getElementById('confPassError').textContent = "";
-    }
-    if(!validateInput(firstname.value, nameRegex) || !validationInput(lastname.value, nameRegex)){
-        console.log("name error");
-        event.preventDefault();
     }
 });
+
 
 
 
