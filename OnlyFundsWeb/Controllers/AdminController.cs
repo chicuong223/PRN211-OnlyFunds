a27d1fdb4a76f516f84835e825b4ce9b8835d9f7
HuyNguyen <ntghuy@gmail.com>
2021-07-08 15:37:57 +0700

    adminpage

--- OnlyFundsWeb/Controllers/AdminController.cs
+++ OnlyFundsWeb/Controllers/AdminController.cs
@@ -13,23 +13,32 @@ namespace OnlyFundsWeb.Controllers
 {
     public class AdminController : Controller
     {
+        private IReportRepository reportRepository = new ReportRepository();
         private IAdminRepository adminRepository = new AdminRepository();
+        private IUserRepository userRepository = new UserRepository();
+        private PRN211_OnlyFunds_CopyContext context = new PRN211_OnlyFunds_CopyContext();
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Login(string username, string password)
+        public IActionResult Login(string username, string password, int? page)
         {
             if (username != null && password != null)
             {
                 Admin user = adminRepository.CheckLogin(username, password);
                 if (user != null)
                 {
+                    TempData["userList"] = userRepository.GetUsers(1);
+                    TempData["reportList"] = reportRepository.GetReports();
+                    TempData["reportSolved"] = context.PostReports.Where(r => r.IsSolved).ToList(); 
+                    TempData["reportPending"] = context.PostReports.Where(r => !r.IsSolved).ToList();
                     HttpContext.Session.SetString("user", username);
                     ViewBag.User = HttpContext.Session.GetString("user");
-                    return View("Success");
+                    IEnumerable<PostReport> reportList = reportRepository.GetReports();
+                   
+                    return View("Success", reportList);
                 }
                 else
                 {
@@ -40,5 +49,71 @@ namespace OnlyFundsWeb.Controllers
             ViewBag.Message = "Error";
             return View("Index");
         }
+
+        public IActionResult UserList(int? page)
+        {
+            if (page == null)
+            {
+                page = 1;
+            }
+
+            int pageSize = 6;
+            int count = context.Users.Count();
+            int end = count / pageSize;
+            IEnumerable<User> userList = userRepository.GetUsers(page.Value);
+            if (count % 6 != 0)
+            {
+                end = end + 1;
+            }
+
+            ViewBag.end = end;
+            return View("Success", userList);
+        }
+
+        public IActionResult ReportSolved(int? page)
+        {
+            if (page == null)
+            {
+                page = 1;
+            }
+
+            int pageSize = 6;
+            int count = context.Users.Count();
+            int end = count / pageSize;
+            IEnumerable<PostReport> reportSolved = context.PostReports.Where(r => r.IsSolved).ToList();
+            ViewBag.end = end;
+            return View("Success", reportSolved);
+        }
+
+        public IActionResult ReportPending(int? page)
+        {
+            if (page == null)
+            {
+                page = 1;
+            }
+
+            int pageSize = 6;
+            int count = context.Users.Count();
+            int end = count / pageSize;
+            IEnumerable<PostReport> reportPending = context.PostReports.Where(r => !r.IsSolved).ToList();
+            ViewBag.end = end;
+            return View("Success", reportPending);
+        }
+
+        public IActionResult TotalReport(int? page)
+        {
+            if (page == null)
+            {
+                page = 1;
+            }
+
+            int pageSize = 6;
+            int count = context.Users.Count();
+            int end = count / pageSize;
+            IEnumerable<PostReport> reports = reportRepository.GetReports();
+            ViewBag.end = end;
+            return View("Success", reports);
+        }
+
     }
 }
